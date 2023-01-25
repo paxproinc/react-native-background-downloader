c00a4b938bf0bf38f2ea9789957fb14a4f88410a
Will <github@will.systems>
2023-01-25 13:43:54 -0600

    fix android addListener/removeListeners warnings
     - warnings at startup:
       - new NativeEventEmitter called with non-null argument without the required addListener method
       - new NativeEventEmitter called with non-null argument without the required removeListeners method
     - related to RN 0.65
     - ref:
       - https://reactnative.dev/docs/native-modules-android#sending-events-to-javascript
       - https://github.com/react-native-netinfo/react-native-netinfo/issues/486
       - https://github.com/react-native-netinfo/react-native-netinfo/pull/487
       - https://github.com/react-native-netinfo/react-native-netinfo/blob/master/CHANGELOG.md#601-2021-08-24
       - https://github.com/react-native-webrtc/react-native-webrtc/pull/1072
       - https://github.com/facebook/react-native/commit/114be1d2170bae2d29da749c07b45acf931e51e2
       - https://github.com/RevenueCat/react-native-purchases/pull/339

--- android/src/main/java/com/eko/RNBackgroundDownloaderModule.java
+++ android/src/main/java/com/eko/RNBackgroundDownloaderModule.java
@@ -197,6 +197,16 @@ public class RNBackgroundDownloaderModule extends ReactContextBaseJavaModule imp
     }
   }
 
+  @ReactMethod
+  public void addListener(String eventName) {
+    // Keep: Required for RN built in Event Emitter Calls.
+  }
+
+  @ReactMethod
+  public void removeListeners(Integer count) {
+    // Keep: Required for RN built in Event Emitter Calls.
+  }
+
   // JS Methods
   @ReactMethod
   public void download(ReadableMap options) {
